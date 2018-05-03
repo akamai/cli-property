4e4e1f80b6057c2003bf1b987103f9f5e4eb8bc4
Jorge Sosa <jorges@oktana.io>
2018-05-03 14:04:13 -0300

    Linted relases.js

--- release.js
+++ release.js
@@ -1,21 +1,20 @@
+'use strict';
 // No command line arguments, we're just going to use the env vars
-const fs = require('fs'),
-  source = 'bin/akamaiProperty',
-  target = 'akamai-property-1.0.2',
-  exec = require('child-process-promise').exec;
+const fs = require('fs');
+const source = 'bin/akamaiProperty';
+const target = 'akamai-property-1.0.2';
+const exec = require('child-process-promise').exec;
 
 exec(`pkg ${source} --target node7-linux-x86,node7-linux-x64,node7-win-x86,node7-win-x64,node7-macos-x64 --output ${target}`)
-  .then(function(result) {
-    let stdout = result.stdout,
-      stderr = result.stderr;
-    console.error('stdout: ', stdout);
-    console.error('stderr: ', stderr);
+  .then(result => {
+    console.error('stdout: ', result.stdout);
+    console.error('stderr: ', result.stderr);
   })
   .then(() => {
     exec(`ls ${target}\*`)
       .then(result => {
         for (let filename of result.stdout.split('\n')) {
-	      if (!filename) {
+	        if (!filename) {
             continue;
           }
           const oldname = filename;
