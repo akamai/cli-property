f29c49a2d4dc2253375b59c0b7a81275c7fdbcba
lucdouwen <luc.douwen@icloud.com>
2018-11-28 11:21:02 +0100

    Update website.js
    See https://github.com/akamai/cli-property/issues/86

--- src/website.js
+++ src/website.js
@@ -368,6 +368,7 @@ class WebSite {
                             "prd_Download_Delivery",
                             "prd_IoT",
                             "prd_Site_Del",
+			    "prd_Site_Accel",
 			    "prd_Fresca"
                         ].indexOf(item.productId)>= 0) {
                             if (productInfo == null) {
